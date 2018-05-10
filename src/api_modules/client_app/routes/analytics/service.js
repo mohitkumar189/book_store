c0f426ca8bb24e40de7462afdbdd46e3de20284d
mohit <mohitkumar.eer@gmail.com>
2018-05-10 14:28:57 +0530

    query executer is added

--- src/api_modules/client_app/routes/analytics/service.js
+++ src/api_modules/client_app/routes/analytics/service.js
@@ -10,116 +10,123 @@ const TABLE_NAME = "user_analytics";
 module.exports = {
     getAll: (pid) => {
         const query = `SELECT * FROM ${TABLE_NAME} WHERE parent_id = ?`;
-        return new Promise((resolve, reject) => {
-            db.getConnection(function (err, connection) {
-                if (err) {
-                    logger.info(err)
-                    connection.release();
-                    reject(err);
-                }
-
-                connection.query(query, pid, function (err, result) {
-                    logger.info("QUERY::" + this.sql);
-                    connection.release();
+        return queryExecuter(query, pid);
+        /*    return new Promise((resolve, reject) => {
+                db.getConnection(function (err, connection) {
                     if (err) {
-                        reject(err)
-                    } else {
-                        resolve(result);
+                        logger.info(err)
+                        connection.release();
+                        reject(err);
                     }
-
-                });
-                connection.on('error', function (err) {
-                    logger.info(err)
-                    connection.release();
-                    reject(err);
-                });
-            })
-        })
+    
+                    connection.query(query, pid, function (err, result) {
+                        logger.info("QUERY::" + this.sql);
+                        connection.release();
+                        if (err) {
+                            reject(err)
+                        } else {
+                            resolve(result);
+                        }
+    
+                    });
+                    connection.on('error', function (err) {
+                        logger.info(err)
+                        connection.release();
+                        reject(err);
+                    });
+                })
+            })*/
     },
     save: (data) => {
         let query = `INSERT INTO ${TABLE_NAME} SET ?`;
-        return new Promise((resolve, reject) => {
-            db.getConnection(function (err, connection) {
-                if (err) {
-                    logger.info(err)
-                    connection.release();
-                    reject(err);
-                }
+        return queryExecuter(query, data);
 
-                connection.query(query, data, function (err, result) {
-                    logger.info("QUERY::" + this.sql);
-                    connection.release();
+        /*     return new Promise((resolve, reject) => {
+                db.getConnection(function (err, connection) {
                     if (err) {
-                        reject(err)
-                    } else {
-                        resolve(result);
+                        logger.info(err)
+                        connection.release();
+                        reject(err);
                     }
-
-                });
-                connection.on('error', function (err) {
-                    logger.info(err)
-                    connection.release();
-                    reject(err);
-                });
-            })
-        })
+    
+                    connection.query(query, data, function (err, result) {
+                        logger.info("QUERY::" + this.sql);
+                        connection.release();
+                        if (err) {
+                            reject(err)
+                        } else {
+                            resolve(result);
+                        }
+    
+                    });
+                    connection.on('error', function (err) {
+                        logger.info(err)
+                        connection.release();
+                        reject(err);
+                    });
+                })
+            }) */
     },
     getById: (id) => {
         const query = `SELECT * FROM ${TABLE_NAME} WHERE id = ?`;
-        return new Promise((resolve, reject) => {
-            db.getConnection(function (err, connection) {
-                if (err) {
-                    logger.info(err)
-                    connection.release();
-                    reject(err);
-                }
+        return queryExecuter(query, id);
 
-                connection.query(query, id, function (err, result) {
-                    logger.info("QUERY::" + this.sql);
-                    connection.release();
-                    if (err) {
-                        reject(err)
-                    } else {
-                        resolve(result);
-                    }
-
-                });
-                connection.on('error', function (err) {
-                    logger.info(err)
-                    connection.release();
-                    reject(err);
-                });
-            })
-        })
+        /*         return new Promise((resolve, reject) => {
+                    db.getConnection(function (err, connection) {
+                        if (err) {
+                            logger.info(err)
+                            connection.release();
+                            reject(err);
+                        }
+        
+                        connection.query(query, id, function (err, result) {
+                            logger.info("QUERY::" + this.sql);
+                            connection.release();
+                            if (err) {
+                                reject(err)
+                            } else {
+                                resolve(result);
+                            }
+        
+                        });
+                        connection.on('error', function (err) {
+                            logger.info(err)
+                            connection.release();
+                            reject(err);
+                        });
+                    })
+                }) */
     },
     updateAtId: (id, data) => {
         data['last_updated'] = common.currentDate();
 
         let query = `UPDATE ${TABLE_NAME} SET ? WHERE id = ${id}`;
-        return new Promise((resolve, reject) => {
-            db.getConnection(function (err, connection) {
-                if (err) {
-                    logger.info(err)
-                    connection.release();
-                    reject(err);
-                }
+        return queryExecuter(query);
 
-                connection.query(query, data, function (err, result) {
-                    logger.info("QUERY::" + this.sql);
-                    connection.release();
-                    if (err) {
-                        reject(err)
-                    } else {
-                        resolve(result);
-                    }
-
-                });
-                connection.on('error', function (err) {
-                    logger.info(err)
-                    connection.release();
-                    reject(err);
-                });
-            })
-        })
+        /*        return new Promise((resolve, reject) => {
+                   db.getConnection(function (err, connection) {
+                       if (err) {
+                           logger.info(err)
+                           connection.release();
+                           reject(err);
+                       }
+       
+                       connection.query(query, data, function (err, result) {
+                           logger.info("QUERY::" + this.sql);
+                           connection.release();
+                           if (err) {
+                               reject(err)
+                           } else {
+                               resolve(result);
+                           }
+       
+                       });
+                       connection.on('error', function (err) {
+                           logger.info(err)
+                           connection.release();
+                           reject(err);
+                       });
+                   })
+               }) */
     }
 }
--- src/middlewares/userValidator.js
+++ src/middlewares/userValidator.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const db = require('../api_modules/client_app/configs/db');
 const _ = require('underscore');
 const TABLE_NAME = "user";
+const queryExecuter = require('../helpers/queryExecuter');
 
 module.exports = async (req, res, next) => {
     if (!_.isEmpty(req.decoded)) {
@@ -12,33 +12,19 @@ module.exports = async (req, res, next) => {
         return next();
     } else {
         const user_id = req.decoded.user_id;
-        const query = `SELECT user_types.type_name AS role FROM ${TABLE_NAME} JOIN user_types ON user.user_type =user_types.id WHERE id = ?`;
-        db.getConnection(function (err, connection) {
-            if (err) {
-                logger.info(err)
-                connection.release();
-                return next(err);
+        const query = `SELECT user_types.type_name AS role FROM ${TABLE_NAME} 
+        JOIN user_types ON user.user_type =user_types.id WHERE id = ?`;
+        try {
+            const result = await queryExecuter(query, user_id);
+            if (result.length > 0) {
+                const userType = result[0]['role'];
+                req.decoded.role = userType;
+                return next();
+            } else {
+                return next(new Error("No user found"));
             }
-            connection.query(query, pid, function (err, result) {
-                logger.info("QUERY::" + this.sql);
-                connection.release();
-                if (err) {
-                    return next(err)
-                } else {
-                    if (result.length > 0) {
-                        const userType = result[0]['role'];
-                        req.decoded.role = userType;
-                        return next();
-                    } else {
-                        return next(new Error("No user found"));
-                    }
-                }
-            });
-            connection.on('error', function (err) {
-                logger.info(err)
-                connection.release();
-                return next(err);
-            });
-        })
+        } catch (error) {
+            return next(error);
+        }
     }
 }
